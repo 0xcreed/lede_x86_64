15b99606eadbe0cf721a47d24293eea8452c099f
0xcreed <43788369+0xcreed@users.noreply.github.com>
2022-02-19 23:22:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 #echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 #echo 'src-git kenz https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-git clone https://github.com/kenzok8/small-package package/small-package
-rm -rf package/small-package/shadowsocks*
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
